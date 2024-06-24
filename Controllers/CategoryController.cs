34710b02d3f1de95d100b508d180b9dc56499f1f
nietbaymuratbaev0999@gmail.com <nietbaymuratbaev0999@gmail.com>
2024-06-24 16:32:50 +0500

    Income And Outcome

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Task1.Data;
+using Task1.Models;
+
+namespace Task1.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private AppDbContext _context;
+        public CategoryController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            List<Category> categories = _context.Category.ToList();
+            return View(categories);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            _context.Category.Add(category);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            Category category = _context.Category.First(x=> x.Id == id);
+            return View(category); 
+        }
+
+
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            _context.Category.Update(category);
+            _context.SaveChanges(); 
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            Category category = _context.Category.First(x=>x.Id == id);
+            _context.Category.Remove(category);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ Models/Transaction.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Task1.Models
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+        public DateTime CreateDate { get; set; } = DateTime.Now;
+        [NotMapped]
+        public Category Category { get; set; } = null!;
+
+        public int CategoryId { get; set; }
+        public int Sum { get; set; }
+        public string? Comment { get; set; }
+
+        [NotMapped] 
+        public List<Category> Categories { get; set; } = new();
+    }
+}
